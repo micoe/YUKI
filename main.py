6670577f395c69e9e676b79da4dd6e4243d4a3e9
micoe <49989575+micoe@users.noreply.github.com>
2020-12-20 10:55:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-import requests,datetime,json,time,re,os
+# -*- coding:utf-8 -*-
+import requests,json,time,re,os
+
+folder = os.environ["FOLDER"]
+sckey = os.environ["SCKEY"]
+phone = os.environ["USERNAME"]
+passWord = os.environ["PASSWORD"]
 
 def allow():
-    all = requests.get(os.environ["FOLDER"], headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0','Referer': 'https://www.lanzous.com/b0ejh22pa', }).text
+    all = requests.get(folder, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0','Referer': 'https://www.lanzous.com/b0ejh22pa', }).text
     if re.search(r'<title>\d</title>',all).group()[7] == '1':return True
     else:return False
 
 def login():
-    data = {"userName": os.environ["USERNAME"],"passWord": os.environ["PASSWORD"],"uatoken": 'byyaohuoid34976'}
+    global s
+    s = requests.session()
+    data = {"userName": phone,"passWord": passWord,"uatoken": 'byyaohuoid34976'}
     s.post('https://yukizq.com/api/yuki/login', headers={'Content-Type': 'application/json'},data=json.dumps(data))
     print('登陆成功')
 
@@ -23,7 +31,6 @@ def receive():
 def query():
     qu = s.post('https://www.yukizq.com/api/yuki/query_receive_task')
     print(qu.json()['data']['message'])
-    time.sleep(3)
     if 'istask' in qu.text:return True
     else:return False
 
@@ -32,22 +39,17 @@ def send():
     a = q.json()['data']['data']
     print(a)
     if a:
-        requests.get('https://sc.ftqq.com/' + os.environ["SCKEY"] + '.send?text=YUKI接到单了点击查看&desp=' + a['createDate'] + '\n\n' + a['pickDate'] + '\n\n' + a['goodprice'] + '\n\n' + a['keyWord'] + '\n\n' + a['remark'] + '\n\n![logo](' + a['picture'] + ')')
+        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=YUKI接到单了点击查看&desp=' + a['createDate'] + '\n\n' + a['pickDate'] + '\n\n' + a['goodprice'] + '\n\n' + a['keyWord'] + '\n\n' + a['remark'] + '\n\n![logo](' + a['picture'] + ')')
         s.post('https://yukizq.com/api/yuki/oktaskremark', headers={'Content-Type': 'application/json'},data=json.dumps({"taskId":a['taskId']}))
 
 
 def main_handler(event, context):
-    wait_until = datetime.datetime.now() + datetime.timedelta(hours=0.24)
-    global break_loop,s
-    s = requests.session()
-    break_loop = False
     if allow():
         login()
-        while not break_loop and status():
+        while status():
             receive()
-            while not break_loop and query():
-                if wait_until < datetime.datetime.now():
-                    break_loop = True
+            while query():
+                time.sleep(3)
         send()
 
 if __name__ == '__main__':
