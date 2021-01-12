21b9e7194b54cdf57425a03b404b0540a76b48f5
micoe <49989575+micoe@users.noreply.github.com>
2021-01-12 17:08:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 import requests,json,time,re,os
 
 folder = os.environ["FOLDER"]
-sckey = os.environ["SCKEY"]
+token = os.environ["TOKEN"]
 phone = os.environ["USERNAME"]
 passWord = os.environ["PASSWORD"]
 
 def allow():
-    all = requests.get(folder, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0','Referer': 'https://www.lanzous.com/b0ejh22pa', }).text
-    if re.search(r'<title>\d</title>',all).group()[7] == '1':return True
+    al = requests.get(folder, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0','Referer': 'https://wwa.lanzoui.com/b05mqj0fi'}).text
+    if re.search(r'<title>\d</title>',al).group()[7] == '1':return True
     else:return False
 
 def login():
@@ -38,7 +38,7 @@ def send():
     q = s.post('https://www.yukizq.com/api/yuki/query_task_1')
     a = q.json()['data']['data']
     if a:
-        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=YUKI接到单了点击查看&desp=' + a['createDate'] + '\n\n' + a['pickDate'] + '\n\n' + str(a['goodprice']) + '\n\n' + a['keyWord'] + '\n\n' + a['remark'] + '\n\n![logo](' + a['picture'] + ')')
+        requests.post('http://pushplus.hxtrip.com/send', data=json.dumps({"token": token, "title": 'YUKI接到单了，点击查看详情',"content": {'接单时间': t(a['createDate']),'开始时间': t(a['pickDate']),'接单账号': a['tbCode'],'商品主图': '<img src="' + a['picture'] + '" alt="商品主图" width="100%"/>','关键词': a['keyWord'],'价格': str(a['goodprice']),'任务说明': a['remark']},"template": "json"}),headers={'Content-Type': 'application/json'})
         s.post('https://yukizq.com/api/yuki/oktaskremark', headers={'Content-Type': 'application/json'},data=json.dumps({"taskId":a['taskId']}))
 
 
